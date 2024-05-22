9a797b581d28bfee8539b4559230f981a6fce890
cameronsmorgan <cameronsmorgan514@gmail.com>
2024-05-22 13:43:01 +0200

    blog index

--- Scripts/animation.js
+++ Scripts/animation.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const funCat = document.getElementById("funCat");
-  let angle = 0;
-  setInterval(() => {
-    angle += 10;
-    funCat.style.transform = `rotate(${angle}deg)`;
-  }, 100);
-});
+
 
 const letterBoxes = document.querySelectorAll("[data-letter-effect]");
 let activeLetterBoxIndex = 0;
--- Scripts/menu.js
+++ Scripts/menu.js
@@ -34,28 +34,40 @@ export function initialise(currentPage){                    //creates nav menu b
     
 }
 
-const blogPosts = [                                          //same stuff as above but only for blogs
-    {name: "Blog 1", href: `${root}/Blogs/blog1.html`},
-    {name: "Blog 2", href: `${root}/Blogs/blog2.html`},
-    {name: "Blog 3", href: `${root}/Blogs/blog3.html`},
-    {name: "Blog 4", href: `${root}/Blogs/blog4.html`},
-    {name: "Blog 5", href: `${root}/Blogs/blog5.html`},
-    {name: "Blog 6", href:`${root}/Blogs/blog6.html`},
-    {name: "Blog 7", href:`${root}/Blogs/blog7.html`},
-    {name: "Blog 8", href:`${root}/Blogs/blog8.html`},
-]
+const blogPosts = [
+    {name: "Blog 1", href: `${root}/Blogs/blog1.html`, thumbnail: `${root}/Images/AliAbdaal.png`},
+    {name: "Blog 2", href: `${root}/Blogs/blog2.html`, thumbnail: `${root}/Images/blog2.png`},
+    {name: "Blog 3", href: `${root}/Blogs/blog3.html`, thumbnail: `${root}/Images/blog3.png`},
+    {name: "Blog 4", href: `${root}/Blogs/blog4.html`, thumbnail: `${root}/Images/blog4.png`},
+    {name: "Blog 5", href: `${root}/Blogs/blog5.html`, thumbnail: `${root}/Images/blog5.png`},
+    {name: "Blog 6", href: `${root}/Blogs/blog6.html`, thumbnail: `${root}/Images/blog6.png`},
+    {name: "Blog 7", href: `${root}/Blogs/blog7.html`, thumbnail: `${root}/Images/blog7.png`},
+    {name: "Blog 8", href: `${root}/Blogs/blog8.html`, thumbnail: `${root}/Images/blog8.png`}
+];
 
 export function blogIndexes() {
-    const ul = document.createElement("ul");
-    for (let blogIndex of blogPosts) {
-        const li = document.createElement("li");
+    const blogList = document.getElementById("blogList");
+    for (let blog of blogPosts) {
+        const card = document.createElement("div");
+        card.className = "blog-card";
+
+        const img = document.createElement("img");
+        img.src = blog.thumbnail;
+        img.alt = blog.name;
+        img.className = "blog-thumbnail";
+
+        const caption = document.createElement("div");
+        caption.className = "blog-caption";
+
         const a = document.createElement("a");
-        a.innerText = blogIndex.name;
-        a.setAttribute("href", blogIndex.href);
-        li.appendChild(a);
-        ul.appendChild(li);
+        a.innerText = blog.name;
+        a.href = blog.href;
+
+        caption.appendChild(a);
+        card.appendChild(img);
+        card.appendChild(caption);
+        blogList.appendChild(card);
     }
-    document.body.appendChild(ul); // Append to the body of the document
 }
 
 
