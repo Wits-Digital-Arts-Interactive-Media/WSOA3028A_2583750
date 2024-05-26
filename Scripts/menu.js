c5fb2b5fd17d8c576728d3f1edf14f5f8f5dfe80
cameronsmorgan <cameronsmorgan514@gmail.com>
2024-05-27 00:39:37 +0200

    Footer

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -47,14 +47,14 @@ export function initialise(currentPage) {                    //creates nav menu 
 }
 
 const blogPosts = [
-    {name: "Blog 1", href: `${root}/Blogs/blog1.html`, thumbnail: `${root}/Images/AliAbdaal.png`},
-    {name: "Blog 2", href: `${root}/Blogs/blog2.html`, thumbnail: `${root}/Images/coffee.png`},
-    {name: "Blog 3", href: `${root}/Blogs/blog3.html`, thumbnail: `${root}/Images/FunnyCat.png`},
-    {name: "Blog 4", href: `${root}/Blogs/blog4.html`, thumbnail: `${root}/Images/AsteroidStorm.png`},
-    {name: "Blog 5", href: `${root}/Blogs/blog5.html`, thumbnail: `${root}/Images/GardenGrove.png`},
-    {name: "Blog 6", href: `${root}/Blogs/blog6.html`, thumbnail: `${root}/Images/PotatoePanic.png`},
-    {name: "Blog 7", href: `${root}/Blogs/blog7.html`, thumbnail: `${root}/Images/Diagram.png`},
-    {name: "Blog 8", href: `${root}/Blogs/blog8.html`, thumbnail: `${root}/Images/Wireframe1.png`}
+    {name: "Blog 1", href: `${root}/Blogs/blog1.html`, thumbnail: `${root}/Images/FunnyCat.png`},
+    {name: "Blog 2", href: `${root}/Blogs/blog2.html`, thumbnail: `${root}/Images/funnycat2.png`},
+    {name: "Blog 3", href: `${root}/Blogs/blog3.html`, thumbnail: `${root}/Images/funnycat3.png`},
+    {name: "Blog 4", href: `${root}/Blogs/blog4.html`, thumbnail: `${root}/Images/funnycat4.png`},
+    {name: "Blog 5", href: `${root}/Blogs/blog5.html`, thumbnail: `${root}/Images/funnycat5.png`},
+    {name: "Blog 6", href: `${root}/Blogs/blog6.html`, thumbnail: `${root}/Images/funnycat6.png`},
+    {name: "Blog 7", href: `${root}/Blogs/blog7.html`, thumbnail: `${root}/Images/funnycat7.png`},
+    {name: "Blog 8", href: `${root}/Blogs/blog8.html`, thumbnail: `${root}/Images/funnycat8.png`}
 ];
 
 export function blogIndexes() {
